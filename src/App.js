bd7db9fb032b98b0b830e25b1c6483bbd91781ad
Roxy Poceros <roxypoceros@gmail.com>
2022-07-01 04:28:49 -0500

    Agregue un add event listener, al dar click al bot□on se refleja su valor en pantalla

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import Screen from './components/Screen';
 import BtnClear from './components/BtnClear';
 
 function App() {
-/* Crear estado para el input se va a mantener como el estado del componente,
-la función lo actualiza e inicia vacío.
-Hay que asignar el imput como el valor de la pantalla en <Screen/> para que
-cuando lo actualicemos se actualice su valor 
-*/
-const [input, setInput]=useState ("")
+  /* Crear estado para el input se va a mantener como el estado del componente,
+  la función lo actualiza e inicia vacío.
+  Hay que asignar el imput como el valor de la pantalla en <Screen/> para que
+  cuando lo actualicemos se actualice su valor 
+  */
+  const [input, setInput] = useState("")
 
-/* addInput será otra función que va a tener un valor y va a ejecutar lo que está entre 
-llaves.  
-la función setInput (de useState) va a actualizar input y entre () le vamos a pasar 
-el nuevo value del input(de useState) concatenado con el nuevo value () y sigo trabajando
-con strings */
-const addInput = value =>{
-  setInput(input + value);
-}
+  /* addInput será otra función que va a tener un valor y va a ejecutar lo que está entre 
+  llaves.  
+  la función setInput (de useState) va a actualizar input y entre () le vamos a pasar 
+  el nuevo value del input(de useState) concatenado con el nuevo value () y sigo trabajando
+  con strings */
+  const addInput = value => {
+    setInput(input + value);
+  }
 
   return (
     <div className="App">
@@ -29,29 +29,33 @@ const addInput = value =>{
         <Screen input={input} />
         {/* filas de calculadora */}
         <div className="row" >
-          {/* se usa etiqueta de cierre porque incluyo el prop entre ellas */}
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button>+</Button>
+          {/* se usa etiqueta de cierre porque incluyo el prop entre ellas. 
+          A cada botón le agrego el prop handleClick para que cada vez que de click
+          en los números, . = se llame a la función addInput dentro del componente
+          (no se usan paréntesis, porque no la quiero ejecutar) 
+          handleClick se define en el botón*/}
+          <Button handleClick={addInput}>1</Button>
+          <Button handleClick={addInput}>2</Button>
+          <Button handleClick={addInput}>3</Button>
+          <Button handleClick={addInput}>+</Button>
         </div>
         <div className="row" >
-          <Button>4</Button>
-          <Button>5</Button>
-          <Button>6</Button>
-          <Button>-</Button>
+          <Button handleClick={addInput}>4</Button>
+          <Button handleClick={addInput}>5</Button>
+          <Button handleClick={addInput}>6</Button>
+          <Button handleClick={addInput}>-</Button>
         </div>
         <div className="row" >
-          <Button>7</Button>
-          <Button>8</Button>
-          <Button>9</Button>
-          <Button>x</Button>
+          <Button handleClick={addInput}>7</Button>
+          <Button handleClick={addInput}>8</Button>
+          <Button handleClick={addInput}>9</Button>
+          <Button handleClick={addInput}>x</Button>
         </div>
         <div className="row" >
-          <Button>=</Button>
-          <Button>0</Button>
-          <Button>.</Button>
-          <Button>/</Button>
+          <Button handleClick={addInput}>=</Button>
+          <Button handleClick={addInput}>0</Button>
+          <Button handleClick={addInput}>.</Button>
+          <Button handleClick={addInput}>/</Button>
         </div>
         <div className="row" >
           <BtnClear>Clear</BtnClear>
